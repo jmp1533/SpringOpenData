ed3af99ec662f41265cfe2009a733e2960f8d78e
Park90 <jmp1533@gmail.com>
2022-08-04 12:34:28 +0900

    최초커밋

---
+++ src/main/java/com/api/opendata/OpendataApplication.java
@@ -0,0 +1,12 @@
+package com.api.opendata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpendataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpendataApplication.class, args);
+	}
+}
---
+++ src/test/java/com/api/opendata/OpendataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.opendata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpendataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
