6d43e626d9bbfe7b2554f2ffcbe7ef51aff48c59
Park90 <jmp1533@gmail.com>
2022-08-31 22:50:36 +0900

    Jsoup 웹크롤링 작업중

--- src/main/java/com/api/opendata/process/Movie.java
+++ src/main/java/com/api/opendata/process/Movie.java
@@ -1,18 +1,16 @@
 package com.api.opendata.process;
 
-import com.api.opendata.common.util.Utility;
-import com.api.opendata.dao.BoxOfficeDao;
-import com.api.opendata.model.boxoffice.DailyBoxOfficeModel;
-import com.api.opendata.model.boxoffice.MovieListModel;
-import com.api.opendata.model.boxoffice.MovieVO;
-import com.api.opendata.model.boxoffice.WeeklyBoxOfficeModel;
+
+import org.springframework.stereotype.Service;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -24,13 +22,57 @@ public class Movie {
     {
         ObjectMapper objectMapper = new ObjectMapper();
         String urlPath = "/movie/running/current.nhn"; // 네이버영화 현재 상영작 예매순위 1~20위
-
         String result = "";
-        result = Utility.GetHttp(_movie_url, urlPath, null);
+
+        Connection conn = Jsoup.connect(_movie_url + urlPath);
+        Document document = null;
+
+        try {
+            document = conn.get();
+
+            getDataList(document);
+
+        } catch (Exception e) {
+
+        }
 
 
         //response = objectMapper.readValue(result, DailyBoxOfficeModel.DailyBoxOfficeResponse.class);
 
         return result;
+    }
+
+    public List<String> getDataList(Document document) {
+        List<String> list = new ArrayList<>();
+        Element movieList = document.selectFirst(".lst_detail_t1");
+
+        for (Element li : movieList.children()) {
+
+            String imgUrl = li.select(".thumb a img").attr("src"); //이미지URL
+
+            String title = li.select(".lst_dsc .tit a").text(); //타이틀
+            String temp = li.select(".lst_dsc .star .tit_t1").next().select(".num").text(); //네이즌 평점
+
+            String  genre = li.select(".lst_dsc dd .info_txt1").next().select(".link_txt a").text();
+            /*String genre = "";
+            boolean runStatus = true;
+            for(Element el : info_txt1.children()){
+                if(false == runStatus){
+                    genre = el.select(".link_txt a").text().replace(" ", ",");
+                    break;
+                }
+
+                if("개요".equals(el.text())){
+                    runStatus = false;
+                }
+            }*/
+
+            System.out.println(title + " | " + genre);
+            System.out.println("평점 : " + temp + "\t" + "예매율 : " + 0 + "%");
+        }
+
+
+
+        return list;
     }
 }
